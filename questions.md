c8d3352f1214cc430cc58ca4f2e59883995185fc
Seandor <seandor123@gmail.com>
2018-05-08 20:41:24 +0800

    add tile and grid class

--- questions.md
+++ questions.md
@@ -1,7 +1,12 @@
 # 学习疑问纪录
 
 ## ES6或TS中还需要用`self = this`吗？
+
 The new fat arrow (=>) feature provides a lexically scoped this rather than a dynamically scoped this. So if you're using that, then you don't need to make a copy of this any more. Otherwise nothing has changed.
+
 ## Angular Component中既然有了`ngOnInit`，那`constructor`还有什么用？
+
 即使Angular定义了ngOnInit，constructor也有其用武之地，其主要作用是注入依赖.
 ngOnInit纯粹是通知开发者组件/指令已经被初始化完成了，此时组件/指令上的属性绑定操作以及输入操作已经完成，也就是说在ngOnInit函数中我们已经能够操作组件/指令中被传入的数据了
+
+## ES6 class里面使用arrow function和使用class property的区别？
--- src/app/g2048-gameboard/g2048-gameboard.component.ts
+++ src/app/g2048-gameboard/g2048-gameboard.component.ts
@@ -35,7 +35,7 @@ export class G2048GameboardComponent implements OnInit {
     console.log('keyCode: ' + $event.keyCode);
   }
 
-  drawBoard = () => {
+  drawBoard () {
     let ctx = this.ctx;
     ctx.save();
     this.drawRoundRect(0, 0, this.oriWidth, this.oriHeight, this.RADIUS);
@@ -44,7 +44,7 @@ export class G2048GameboardComponent implements OnInit {
     ctx.restore();
   };
 
-  setCanvasSize = (size) => {
+  setCanvasSize (size) {
     const cols = size.width;
     const rows = size.height;
 
@@ -77,7 +77,7 @@ export class G2048GameboardComponent implements OnInit {
     }
   };
 
-  drawRoundRect = (startX, startY, width, height, radius) => {
+  drawRoundRect (startX, startY, width, height, radius) {
     let ctx = this.ctx;
     ctx.beginPath();
     ctx.arc(startX + radius, startY + radius, radius, Math.PI, Math.PI * 3 / 2);
@@ -90,3 +90,187 @@ export class G2048GameboardComponent implements OnInit {
     ctx.lineTo(startX, startY + radius);
   };
 }
+
+interface Position {
+  row: number,
+  col: number
+}
+
+class Tile {
+  private row: number;
+  private col: number;
+  private value: number;
+  private previousPosition: Position;
+
+  constructor(position: Position, value: number) {
+    this.row = position.row;
+    this.col = position.col;
+    this.value = value || 2;
+
+    // for animation
+    // mergedFrom saves the other tile's position which has merged to the tile.
+    // this.previousPosition = null;
+    // this.mergedFrom = null;
+  }
+
+  getPosition () {
+    return { row: this.row, col: this.col };
+  }
+
+  savePosition () {
+    this.previousPosition = { row: this.row, col: this.col };
+  }
+
+  updatePostion (position) {
+    this.row = position.row;
+    this.col = position.col;
+  };
+
+  serialize () {
+    return {
+      position: {
+        row: this.row,
+        col: this.col
+      },
+      value: this.value
+    };
+  };
+}
+
+class Grid {
+  gridWidth: number;
+  gridHeight: number;
+  cells;
+
+  constructor (gridWidth, gridHeight) {
+    this.gridWidth = gridWidth;
+    this.gridHeight = gridHeight;
+    this.cells = this.init();
+  }
+
+  // this.cells is just a 2-D Array
+  init () {
+    var row, idx, jdx;
+    var cells = [];
+    for (idx = 0; idx < this.gridHeight; idx++) {
+      row = [];
+      for (jdx = 0; jdx < this.gridWidth; jdx++) {
+        row[jdx] = null;
+      }
+      cells[idx] = row;
+    }
+    return cells;
+  };
+
+  getWidth () {
+    return this.gridWidth;
+  };
+
+  getHeight () {
+    return this.gridHeight;
+  };
+
+  // Given a direction vector and start cell to traverse the grid
+  // return a list of tiles
+  traverseAndSetTile (startPos, directionVector, numSteps, mergedLine) {
+    // 0: UP 1: RIGHT 2: DOWN 3: LEFT
+    var row, col;
+    var traversal = [];
+    var position;
+    for (var i = 0; i < numSteps; i++) {
+      row = startPos.row + i * directionVector.row;
+      col = startPos.col + i * directionVector.col;
+      if (mergedLine === undefined) {
+        // only traverse
+        traversal.push(this.cells[row][col]);
+      } else {
+        // set tile
+        if (mergedLine[i] !== null) {
+          position = { row: row, col: col };
+          mergedLine[i].savePosition();
+          mergedLine[i].updatePostion(position);
+          this.setTile(mergedLine[i]);
+        } else {
+          this.cells[row][col] = null;
+        }
+      }
+    }
+    return traversal;
+  };
+
+  // Find an available random cell position
+  randomAvailableCells () {
+    var cells = this.availableCells();
+    if (cells.length) {
+      return cells[Math.floor(Math.random() * cells.length)];
+    }
+  };
+
+  // Return a list of available cell index
+  availableCells () {
+    var cells = [];
+    this.eachCell(function (row, col, tile) {
+      if (tile === null) {
+        cells.push({ row: row, col: col });
+      }
+    });
+    return cells;
+  };
+
+  // Return true if there is cell available
+  cellsAvailable () {
+    for (var row = 0; row < this.gridHeight; row++) {
+      for (var col = 0; col < this.gridWidth; col++) {
+        if (!this.cells[row][col]) {
+          return true;
+        }
+      }
+    }
+  };
+
+  // iteration of the grid
+  eachCell (callback) {
+    for (var row = 0; row < this.gridHeight; row++) {
+      for (var col = 0; col < this.gridWidth; col++) {
+        callback(row, col, this.cells[row][col]);
+      }
+    }
+  };
+
+  setTile (tile) {
+    this.cells[tile.row][tile.col] = tile;
+  };
+
+  // return null if there is no tile
+  getTile (position) {
+    if (this.withinBounds(position)) {
+      return this.cells[position.row][position.col];
+    } else {
+      return null;
+    }
+  };
+
+  withinBounds (position) {
+    return position.row >= 0 && position.row < this.gridHeight &&
+      position.col >= 0 && position.col < this.gridWidth;
+  };
+
+  serialize () {
+    var row, idx, jdx;
+    var cells = [];
+    for (idx = 0; idx < this.gridHeight; idx++) {
+      row = [];
+      for (jdx = 0; jdx < this.gridWidth; jdx++) {
+        row.push(this.cells[idx][jdx] ? this.cells[idx][jdx].serialize() : null);
+      }
+      cells[idx] = row;
+    }
+    return {
+      width: this.gridWidth,
+      height: this.gridHeight,
+      cells: this.cells
+    };
+  };
+}
+
+
